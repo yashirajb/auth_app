34f64d94736431d3a7e8a0b7cbaccbd2efb71acf
Yashira Baggett <Yashira@Yashiras-Air.attlocal.net>
2021-03-22 20:15:02 -0700

    added app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+// const express = require('express');
+
+// const app = express();
+
+// app.listen(3000); 
+
+
+const express = require('express');
+
+const app = express();
+
+// route
+
+app.get('/', (req, res, next) => {
+    res.send(`<h1>Hello World</h1>`)
+}); 
+
+app.listen(3000); 
